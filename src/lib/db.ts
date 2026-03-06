21fd9461003d721f0267ed1058c0a835e6e60e21
983kk <haydyncm@gmail.com>
2026-03-06 17:00:58 +0000

    Hardcoded the suparbase URL

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -7,7 +7,7 @@
 import { createClient } from '@supabase/supabase-js';
 
 // ── Supabase Client ─────────────────────────────────────────────
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseUrl = 'https://zkoibcpekzqvvgbijvtl.supabase.co';
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 export const supabase = createClient(supabaseUrl, supabaseKey);
 
